324ca64bfc7a27fa4a79207122669e65c9dc7a83
Anjana Silva <anjanasilva@users.noreply.github.com>
2020-12-28 12:41:02 +0000

    Added comments

--- TypeScript/BinarySearch.ts
+++ TypeScript/BinarySearch.ts
@@ -1,6 +1,12 @@
 const sortedArray = [1, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59];
 console.log(binarySearch(sortedArray, 3));
 
+/**
+ * Takes a sorted array, target value and return the index of the target value in the array
+ * @param sortedArray Sorted array
+ * @param target Target value to search
+ * @returns Return the index or -1 if not found in the array
+ */
 function binarySearch(sortedArray: number[], target: number): number {
   let startIndex = 0;
   let endIndex = sortedArray.length - 1;
